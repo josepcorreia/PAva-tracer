ccbbf9c5c7d9125a17574f93b3f50cefcb895047
Jose Miguel Pinheiro Correia <zemiguel91@gmail.com>
2014-04-30 11:06:25 +0100

    minor changes

--- src/ist/meic/pa/TraceVM.java
+++ src/ist/meic/pa/TraceVM.java
@@ -1,7 +1,22 @@
 package ist.meic.pa;
 
+import javassist.ClassPool;
+import javassist.CtClass;
+import javassist.NotFoundException;
+
 public class TraceVM {
 	public static void main(String[] args){
 		
+		ClassPool pool = ClassPool.getDefault();
+		CtClass cc = null;
+		try {
+			if(args[0] != null) {				
+				cc = pool.get(args[0]);
+			}
+			System.out.println(cc);
+		} catch (NotFoundException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
 	}
 }
