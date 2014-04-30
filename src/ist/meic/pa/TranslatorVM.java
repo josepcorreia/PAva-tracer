1bb756f021da43ffda88a51b0ae01207ed8bd667
Jose Miguel Pinheiro Correia <zemiguel91@gmail.com>
2014-04-30 18:48:28 +0100

    cenas

--- src/ist/meic/pa/TranslatorVM.java
+++ src/ist/meic/pa/TranslatorVM.java
@@ -3,8 +3,14 @@ package ist.meic.pa;
 import javassist.CannotCompileException;
 import javassist.ClassPool;
 import javassist.CtClass;
+import javassist.CtMethod;
 import javassist.NotFoundException;
 import javassist.Translator;
+import javassist.bytecode.BadBytecode;
+import javassist.bytecode.CodeAttribute;
+import javassist.bytecode.CodeIterator;
+import javassist.bytecode.MethodInfo;
+import javassist.bytecode.Mnemonic;
 
 public class TranslatorVM implements Translator {
 
@@ -19,8 +25,20 @@ public class TranslatorVM implements Translator {
 			if(arg1 != null) {				
 				cc = arg0.get(arg1);
 			}
-			System.out.println(cc);
+		
+			CtMethod method = cc.getDeclaredMethod("foo");
+			MethodInfo minfo = method.getMethodInfo();
+			CodeAttribute ca = minfo.getCodeAttribute();
+			CodeIterator ci = ca.iterator() ;
+			while (ci.hasNext()) {
+			    int index = ci.next();
+			    int op = ci.byteAt(index);
+			    System.err.println(Mnemonic.OPCODE[op]);
+			}
 		} catch (NotFoundException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		} catch (BadBytecode e) {
 			// TODO Auto-generated catch block
 			e.printStackTrace();
 		}
