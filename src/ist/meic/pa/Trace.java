6dffe2ef0d6870f1dd5e726d84204c222e6b45b0
Antonio Goulao <goulao80.3@gmail.com>
2014-05-03 18:58:30 +0100

    Add object when passed as parameter and as return of a method

--- src/ist/meic/pa/Trace.java
+++ src/ist/meic/pa/Trace.java
@@ -6,11 +6,12 @@ import java.util.HashMap;
 public class Trace {
 	private static HashMap<Object, ArrayList<String>> info = new HashMap<Object, ArrayList<String>>();
 	
-	public static void addObject(Object o, String s){
+	public static void addObject(Object o){
 		info.put(o, new ArrayList<String>());
 	}
 	
 	public static void addInfo(Object o, String s){
+		// TODO Descobrir o bug que substitui o objecto actual pelo novo
 		info.get(o).add(s);
 	}
 	
@@ -18,6 +19,7 @@ public class Trace {
 		if(!info.containsKey(o))
 			System.err.println("Tracing for " + o.toString() + " is nonexistent!");		
 		else{
+			System.err.println("Tracing for " + o.toString());
 			for(String s: info.get(o)){
 				System.err.println(s);
 			}
--- src/ist/meic/pa/TranslatorVM.java
+++ src/ist/meic/pa/TranslatorVM.java
@@ -11,13 +11,14 @@ import javassist.bytecode.CodeAttribute;
 import javassist.bytecode.CodeIterator;
 import javassist.bytecode.MethodInfo;
 import javassist.bytecode.Mnemonic;
+import javassist.expr.ExprEditor;
 import javassist.expr.MethodCall;
 
 public class TranslatorVM implements Translator {
 
 	@Override
 	public void onLoad(ClassPool arg0, String arg1) throws NotFoundException,
-			CannotCompileException {
+	CannotCompileException {
 		// TODO Auto-generated method stub
 		System.err.println("A carregar a classe: "+ arg1);
 
@@ -26,13 +27,24 @@ public class TranslatorVM implements Translator {
 			if(arg1 != null) {				
 				cc = arg0.get(arg1);
 			}
-			
+			if(cc.getName().equals("ist.meic.pa.Trace"))
+				return;
+
 			CtMethod[] methods = cc.getDeclaredMethods();
-				for(CtMethod m : methods){
-					System.err.println(m.toString());
-				}
-			
-				
+			for(CtMethod m : methods){
+				m.instrument(
+						new ExprEditor() {
+							public void edit(MethodCall m)
+									throws CannotCompileException
+									{
+								m.replace("{  if($args.length > 0) { ist.meic.pa.Trace.addObject($args[0]); " +
+										"ist.meic.pa.Trace.addInfo($args[0], \"->\");} " +
+										"$_ = $proceed($$); " +
+										"if($_ != null){ ist.meic.pa.Trace.addObject($_); " +
+										"ist.meic.pa.Trace.addInfo($_, \"<-\");}}");
+									}
+						});
+			}
 		} catch (NotFoundException e) {
 			// TODO Auto-generated catch block
 			e.printStackTrace();
@@ -40,9 +52,9 @@ public class TranslatorVM implements Translator {
 	}
 	@Override
 	public void start(ClassPool arg0) throws NotFoundException,
-			CannotCompileException {
+	CannotCompileException {
 		// TODO Auto-generated method stub
-		
+
 	}
 
 }
