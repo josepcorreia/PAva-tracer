a23b1ed41d76c5a1a2636b241ff77d89d617a79f
Antonio Goulao <goulao80.3@gmail.com>
2014-05-11 17:46:20 +0100

    Add methods to correctly handle primitive type returns. Fix spaces on strings to be stored with the object.

--- src/ist/meic/pa/Util.java
+++ src/ist/meic/pa/Util.java
@@ -1,15 +1,12 @@
 package ist.meic.pa;
 
-import java.util.HashSet;
-import java.util.Set;
-
 public class Util {
 	public static void processArguments(Object[] args, String filename, String methodname, String line){
 		 if(args.length > 0) { 
 			for(int i=0; i < args.length; i++){ 
 				if(!args[i].getClass().isPrimitive()){
 					ist.meic.pa.Trace.addObject(args[i]);
-					ist.meic.pa.Trace.addInfo(args[i], "-> " + methodname + " on " + filename + ":" + line);
+					ist.meic.pa.Trace.addInfo(args[i], "  -> " + methodname + " on " + filename + ":" + line);
 				}
 			}
 		 }
@@ -17,12 +14,32 @@ public class Util {
 	public static void processReturn(Object ret, String filename, String methodname, String line){
 		if(ret != null){ 
 			ist.meic.pa.Trace.addObject(ret); 
-			ist.meic.pa.Trace.addInfo(ret, "<- " + methodname +" on " + filename + ":" + line);
+			ist.meic.pa.Trace.addInfo(ret, "  <- " + methodname +" on " + filename + ":" + line);
 		}
 	}
 	public static void processReturn(boolean ret, String filename, String methodname, String line){
-			ist.meic.pa.Trace.addObject(ret); 
-			ist.meic.pa.Trace.addInfo(ret, "<- " + methodname +" on " + filename + ":" + line);
+		// DO NOTHING
+	}
+	public static void processReturn(int ret, String filename, String methodname, String line){
+		// DO NOTHING
+	}
+	public static void processReturn(double ret, String filename, String methodname, String line){
+		// DO NOTHING
+	}
+	public static void processReturn(float ret, String filename, String methodname, String line){
+		// DO NOTHING
+	}
+	public static void processReturn(short ret, String filename, String methodname, String line){
+		// DO NOTHING
+	}
+	public static void processReturn(long ret, String filename, String methodname, String line){
+		// DO NOTHING
+	}
+	public static void processReturn(char ret, String filename, String methodname, String line){
+		// DO NOTHING
+	}
+	public static void processReturn(byte ret, String filename, String methodname, String line){
+		// DO NOTHING
 	}
 }
 
